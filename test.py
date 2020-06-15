8879f317ccecf0810d6b25dd3529588eea68bb02
Qing <qing@dhcp-wlan-uzh-10-12-40-220.uzh.ch>
2020-06-15 14:49:13 +0200

    test performance script

--- test.py
+++ test.py
@@ -1,65 +1,66 @@
-import os
-import numpy as np
 import time
 from reachability import *
 from collections import defaultdict
 from reader import *
 import random
+import numpy as np
 
 if __name__ == '__main__':
     folder = 'datasets'
-
-    # filename = 'soc-Epinions1.txt'
-    # filename = 'moreno_blogs'
     filename = 'moreno_blogs'
-
-    # reader = open(os.path.join(folder, filename), 'r')
-
-    # N = 76000
     N, records = readMoreno(folder, filename)
+    #M = np.zeros([N, N], dtype = int)
     L = defaultdict(set)
     for (node1, node2) in records:
         # nodes = line.rstrip().split('\t')
-        # M[int(nodes[0])][int(nodes[1])] = 1
+        #M[node1][node2] = 1
         L[node1].add(node2)
-
-    print("finishing building adjacency list.")
+    print("Building the maximum 2-hop labeling.")
     start = time.time()
-    # maxL = maximumLabelingM(M)
+    #bfsl(L, N, 1)
     maxL = maximumLabelingL(L, N)
-    # res = BFSL(L, N, 1)
     end = time.time()
-    print("build max Labeling takes ", end - start, ' seconds.')
+    print(end - start, ' seconds.')
+    sum_in = 0
+    sum_out = 0
+    RL = [defaultdict(set) for _ in range(N)]
+    ratio_list = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]
+    for ratio in ratio_list:
+        size = int(ratio * N)
+        total_out = 0
+        total_in = 0
+        sample_out = 0
+        sample_in = 0
+        for i in range(len(maxL)):
+            if len(maxL[i]['in']) > size:
+                RL[i]['in'] = set(random.choices(list(maxL[i]['in']), k = size))
+            else:
+                RL[i]['in'] = maxL[i]['in'].copy()
 
-    count = 0
-    trials = 40
-    T = 0
-    for i in range(N):
-        maxNum = max(trials, len(L[i]))
-        if maxNum <= trials:
-            for j in L[i]:
-                count += 1
-                start = time.time()
-                deleteL(L, maxL, N, i, j)
-                T += (time.time() - start)
-                L[i].add(j)
-                if count % 100 == 0:
-                    print(count, T / count)
-        else:
-            candidates = set()
-            for c in L[i]:
-                candidates.add(c)
-            for _ in range(trials):
-                t_index = random.randint(0, len(candidates) - 1)
-                candidates_list = list(candidates)
-                t = candidates_list[t_index]
-                candidates.remove(t)
-                count += 1
-                start = time.time()
-                deleteL(L, maxL, N, i, t)
-                T += (time.time() - start)
-                L[i].add(t)
-                if count % 100 == 0:
-                    print(count, T / count)
+            if len(maxL[i]['out']) > size:
+                RL[i]['out'] = set(random.choices(list(maxL[i]['out']), k = size))
+            else:
+                RL[i]['out'] = maxL[i]['out'].copy()
 
-    print(count, T / count)
+            total_out += len(maxL[i]['out'])
+            total_in += len(maxL[i]['in'])
+            sample_out += len(RL[i]['out'])
+            sample_in += len(RL[i]['in'])
+        print("ratio: %f" % ratio)
+        print(
+            "total size of Lin: %d, total size of Lout: %d, sum of sample size of Lin: %d, sum of sample size of Lout: %d." % (
+            total_out, total_in, sample_in, sample_out))
+        count = 0
+        hit = 0
+
+        for u in range(N):
+            for v in range(N):
+                if len(maxL[u]['out'] & maxL[v]['in']) == 0:
+                    continue
+                count += 1
+                if len(RL[u]['out'] & RL[v]['in']) > 0:
+                    hit += 1
+
+        print('hit: %d, count: %d' % (hit, count))
+        print('precission: %f' % (hit / count))
+        print()
