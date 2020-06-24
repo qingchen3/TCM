06ee4f6448fb9d35918d384a7cb3d8776e84ba1f
Qing <qing@dhcp-wlan-uzh-10-12-163-161.uzh.ch>
2020-06-24 14:54:29 +0200

    add more readers

--- reader.py
+++ reader.py
@@ -1,6 +1,55 @@
 import os
 
 
+def readTest(folder, filename):
+    reader = open(os.path.join(folder, filename), 'r')
+    N = 10
+    records = []
+    for line in reader:
+        nodes = line.rstrip().split(' ')
+        records.append((int(nodes[0]), int(nodes[1])))
+
+    return N, records
+
+def readChicagoRegional(folder, filename):
+    reader = open(os.path.join(folder, filename), 'r')
+    N = 1468
+    records = []
+    for line in reader:
+        if '%' in line:
+            continue
+        nodes = line.rstrip().split(' ')
+        records.append((int(nodes[0]), int(nodes[1])))
+        records.append((int(nodes[1]), int(nodes[0])))
+
+    return N, records
+
+def readAirTraffic(folder, filename):
+    reader = open(os.path.join(folder, filename), 'r')
+    N = 1227
+    records = []
+    for line in reader:
+        if '%' in line:
+            continue
+        nodes = line.rstrip().split(' ')
+        records.append((int(nodes[0]), int(nodes[1])))
+
+    return N, records
+
+
+def readUSAirport(folder, filename):
+    reader = open(os.path.join(folder, filename), 'r')
+    N = 1580
+    records = []
+    for line in reader:
+        if '%' in line:
+            continue
+        nodes = line.rstrip().split(' ')
+        records.append((int(nodes[0]), int(nodes[1])))
+
+    return N, records
+
+
 def readEpinion(folder, filename):
     reader = open(os.path.join(folder, filename), 'r')
     N = 76000
@@ -24,11 +73,24 @@ def readHepPh(folder, filename):
 
     return N, records
 
+
 def readMoreno(folder, filename):
     reader = open(os.path.join(folder, filename), 'r')
     N = 1225
     records = []
     for line in reader:
+        nodes = line.rstrip().split(' ')
+        records.append((int(nodes[0]), int(nodes[1])))
+
+    return N, records
+
+def readMorenoHighSchool(folder, filename):
+    reader = open(os.path.join(folder, filename), 'r')
+    N = 71
+    records = []
+    for line in reader:
+        if '%' in line:
+            continue
         nodes = line.rstrip().split(' ')
         records.append((int(nodes[0]), int(nodes[1])))
 
